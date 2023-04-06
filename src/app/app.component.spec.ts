e56dbebf307a30b6000c8483098e71a53c99bddc
bacallaV <valdohalo3@hotmail.com>
2023-04-06 12:01:56 -0600

    Cambio de nombre del proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-pacto'`, () => {
+  it(`should have as title 'yo-recomiendo-zacatecas-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-pacto');
+    expect(app.title).toEqual('yo-recomiendo-zacatecas-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('web-pacto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('yo-recomiendo-zacatecas-app app is running!');
   });
 });
