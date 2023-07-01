5d74ccd10b3e058f35aed339892f450a20ea1901
bacallaV <valdohalo3@hotmail.com>
2023-07-01 14:52:21 -0600

    Fixed SearchBar styles

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  @Input() locateSearchButtonAtBottom: boolean = false;
+  @Input() initialValue: string = '';
+  @Input() placeHolder: string = 'Buscar lugar';
 
+  public query: string = '';
+  public zone: string = '';
+
+  @Output() onSearch: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    this.query = this.initialValue;
+  }
+
+  public search(): void {
+    this.onSearch.emit({
+      query: this.query,
+      zone: this.zone,
+    });
+  }
 }
